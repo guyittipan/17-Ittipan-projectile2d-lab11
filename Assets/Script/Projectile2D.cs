41738b853a7d07d2ecff7f646fdf3a4568d2fe57
66110037 <66110037@kmitl.ac.th>
2025-05-01 17:17:01 +0700

    Final

--- Assets/Script/Projectile2D.cs
+++ Assets/Script/Projectile2D.cs
@@ -3,26 +3,39 @@ using UnityEngine;
 public class Projectile2D : MonoBehaviour
 {
     [SerializeField] Transform shootPoint;
-    [SerializeField] GameObject target;         // target sprite
+    [SerializeField] GameObject target;
     [SerializeField] Rigidbody2D bulletPrefab;
 
     void Update()
     {
         if (Input.GetMouseButtonDown(0))
         {
-            // shoot raycast to detect mouse clicked position
             Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
             Debug.DrawRay(ray.origin, ray.direction * 5f, Color.red, 5f);
 
-            // get click point
             RaycastHit2D hit = Physics2D.GetRayIntersection(ray, Mathf.Infinity);
 
-            // if hit object with collider
             if (hit.collider != null)
             {
                 target.transform.position = new Vector2(hit.point.x, hit.point.y);
                 Debug.Log("hit " + hit.collider.name);
+
+                Vector2 projectileVelocity = CalculateProjectileVelocity(shootPoint.position, hit.point, 1f);
+
+                Rigidbody2D shootBullet = Instantiate(bulletPrefab, shootPoint.position, Quaternion.identity);
+
+                shootBullet.linearVelocity = projectileVelocity;
             }
         }
+    }
+
+    Vector2 CalculateProjectileVelocity(Vector2 origin, Vector2 target, float time)
+    {
+        Vector2 distance = target - origin;
+
+        float velocityX = distance.x / time;
+        float velocityY = distance.y / time + 0.5f * Mathf.Abs(Physics2D.gravity.y) * time;
+
+        return new Vector2(velocityX, velocityY);
     }
 }
